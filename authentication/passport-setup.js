3feafb9e089926f29eb79aa7493342d1b118a07c
Your Name <you@example.com>
2024-11-02 23:02:26 +0700

    add edit page and save it into database by html form and authenticate by gg, fb,github

--- authentication/passport-setup.js
+++ authentication/passport-setup.js
@@ -1,6 +1,7 @@
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import dotenv from 'dotenv';  
+dotenv.config(); 
 
 passport.serializeUser((user, done) => {
     done(null, user.id);
